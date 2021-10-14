5b9aab993e75f9dc73778066960a6f2ce857d214
gareth <gjmorris3@gmail.com>
2021-10-14 17:56:28 +0100

    Initial commit

---
+++ thinkful-logic-drills-traffic-light.js
@@ -0,0 +1,10 @@
+function updateLight(current) {
+  
+    if (current == "green"){
+      return "yellow"
+    }
+     if (current == "yellow"){
+       return "red"
+     }
+     return "green"
+   }
